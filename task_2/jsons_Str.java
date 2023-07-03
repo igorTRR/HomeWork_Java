0d7d2dc92c086c1a7a9003360a6dee5d94a440f3
Igor <tararuev65@gmail.com>
2023-07-03 21:30:35 +0600

    Dd_2_Json

---
+++ task_2/jsons_Str.java
@@ -0,0 +1,31 @@
+package task_2;
+
+
+public class jsons_Str {
+    private static final String json = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +            
+    "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +            
+    "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";    
+    private static final String El1 = "Студент ";    
+    private static final String El2 = " получил ";    
+    private static final String El3 = " по предмету ";    
+    public static void main(String[] args) {        
+        StringBuilder sb = new StringBuilder();        
+        String newjson = json.replaceAll("\\{", "").replaceAll("\\}", "").replaceAll("\"", "").replace('[', ' ').replace(']', ' ');        
+        //System.out.println(newjson);        
+        String[] parts = newjson.split(",");        
+        for (String part : parts){            
+            String[] params = part.split(":");            
+            if("фамилия".equals(params[0].trim())) 
+            {                
+                sb = new StringBuilder(El1).append(params[1]);            
+            }            else if("оценка".equals(params[0].trim())) sb.append(El2).append(params[1]);            
+            else if("предмет".equals(params[0].trim())) {                
+                sb.append(El3).append(params[1]);                
+                System.out.println(sb);          
+            }  
+              } 
+             
+}
+}
+
+
